f3e9bfaea9b41fc02e397ce2ccad44ef467eb148
zzr <1763313803@qq.com>
2023-03-14 22:43:24 +0800

    first commit

---
+++ src/main/java/com/zzr/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.zzr.git;
+
+/**
+ * @author zzr
+ * @date 2023/3/14
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
